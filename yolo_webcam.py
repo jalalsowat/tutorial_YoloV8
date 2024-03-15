d719b13bd2837bd8c702ccb4342848b9efa5fc1a
Jalal0303 <Jalal0303@users.noreply.github.com>
2024-03-15 15:39:29 +0300

    Add box classname conf

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -1,13 +1,49 @@
+import math
+import cvzone
 from ultralytics import YOLO
 import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+classname = [
+    "person", "bicycle", "car", "motorcycle", "airplane",
+    "bus", "train", "truck", "boat", "traffic light",
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird",
+    "cat", "dog", "horse", "sheep", "cow",
+    "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee",
+    "skis", "snowboard", "sports ball", "kite", "baseball bat",
+    "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+    "wine glass", "cup", "fork", "knife", "spoon",
+    "bowl", "banana", "apple", "sandwich", "orange",
+    "broccoli", "carrot", "hot dog", "pizza", "donut",
+    "cake", "chair", "couch", "potted plant", "bed",
+    "dining table", "toilet", "TV", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven",
+    "toaster", "sink", "refrigerator", "book", "clock",
+    "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+]
 
-model = YOLO
+
+model = YOLO(r"C:\Users\jalal\PycharmProjects\YoloV8_tutorial\yolov8n.pt")
 
 while True :
     success , img = cap.read()
+    results = model(img,stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+            w , h = x2-x1 , y2-y1
+            cvzone.cornerRect(img , (w,h,x1,y1))
+            conf = math.ceil((box.conf[0]*100))/100
+            cls  = int(box.cls[0])
+            cvzone.putTextRect(img,f"{classname[cls]} {conf}",(max(50,x1),max(200,y1)),scale=0.7,thickness=1)
+
+
+
+
     cv2.imshow("webcam", img)
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
